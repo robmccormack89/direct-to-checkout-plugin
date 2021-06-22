ea841cdcc0ae2cfa4ce471b24550ebd845a3572c
robmccormack89 <robertmccormack147@gmail.com>
2021-06-22 22:06:58 +0100

    better wc_get_template

--- inc/DirectToCheckout.php
+++ inc/DirectToCheckout.php
@@ -2,14 +2,6 @@
 namespace Rmcc;
 use Timber\Timber;
 
-/**
- * DIRECT TO CHECKOUT
- * this plugin uses it's own timber integration, as opposed to piggybacking off the timber of the theme
- *
- * 1. don't set basket page in Woocommerce Settings -> Advanced.
- *
-**/
-
 class DirectToCheckout extends Timber {
 
   public function __construct() {
@@ -19,40 +11,44 @@ class DirectToCheckout extends Timber {
     add_filter('timber/twig', array($this, 'add_to_twig'));
     add_filter('timber/context', array($this, 'add_to_context'));
     
-    // after plugins are loaded, do the checkout stuff.
-    // this is so other plugins have a chance to do their stuff first
-    add_action('plugins_loaded', array($this, 'direct_to_checkout'));
+    // after plugins are loaded, do the checkout stuff. this is so other plugins have a chance to do their stuff first, including Woo!
+    add_action('plugins_loaded', array($this, 'on_plugins_loaded'));
     
-    // override the template (page.php) used for the checkout page
+    // override the template used for the checkout page. would be page.php from the theme but for this
     add_filter('page_template', array($this, 'direct_to_checkout_template'));
     
-    // remove custom add_custom_demo_store_notice from custom store notice plugin only on checkout. use teplate redirect to access is_checkout()
-    add_action('template_redirect', array($this, 'remove_custom_store_notice_on_checkout'));
+    // removes add_custom_demo_store_notice from Custom Store Notice plugin, but only on checkout.
+    // we use use template_redirect to access is_checkout(). see https://codex.wordpress.org/Plugin_API/Action_Reference for more actions that can be used
+    // this is useful for removing functions added to actions by other plugins
+    // such functions which are added to theme via action hooks, need to be written outside the class
+    // that way they can be removed as in remove_custom_store_notice_from_plugin_on_checkout
+    add_action('template_redirect', array($this, 'remove_custom_store_notice_from_plugin_on_checkout'));
+    
+    // allow plugin's woo templates to override others
+    add_filter('wc_get_template', array($this, 'wc_get_template'), 10, 5); 
   }
   
-  public function remove_custom_store_notice_on_checkout() {
-    if(is_checkout()){
-      remove_action('rmcc_before_header', 'add_custom_demo_store_notice', 10);
-    }
+  // timber stuff. the usual stuff
+  public function add_to_twig($twig) { 
+    $twig->addExtension(new \Twig_Extension_StringLoader());
+    return $twig;
+  }
+  public function add_to_context($context) {
+    $context['direct_to_checkout_url'] = DIRECT_TO_CHECKOUT_URL;
+    return $context;    
   }
   
-  public function direct_to_checkout_template($page_template) {
-    // if is the checkout page, use the new template
-    if (is_checkout()) {
-      $page_template = DIRECT_TO_CHECKOUT_PATH . 'templates/direct-to-checkout-template.php';
-    }
-    return $page_template;
-  }
-  
-  public function direct_to_checkout() {
+  // after plugins are loaded, do the checkout stuff. this is so other plugins have a chance to do their stuff first, including Woo!
+  public function on_plugins_loaded() {
+    
     // remove basket button from minicart
     remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_button_view_cart', 10);
     
     // remove payments from order review
     remove_action('woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20);
     
-    // allow plugin's woo templates to override theme's
-    add_filter('wc_get_template', array($this, 'wc_get_template'), 10, 5); 
+    // disable order notes
+    add_filter('woocommerce_enable_order_notes_field', '__return_false');
     
     // after logging in via woo login form, if cart has contents, go to checkout, else go to shop
     add_filter('woocommerce_login_redirect', array($this, 'checkout_redirect_login'));
@@ -60,20 +56,8 @@ class DirectToCheckout extends Timber {
     // redirect to checkout from single product's add to cart; without the messages
     add_filter('add_to_cart_redirect', array($this, 'redirect_to_checkout'));
     
-    // disable order notes
-    add_filter('woocommerce_enable_order_notes_field', '__return_false');
-    
     // allows removal of products in checkout
     add_filter('woocommerce_cart_item_name', array($this, 'woo_checkout_remove_item'), 10, 3);
-    
-    // remove checkout fields
-    add_filter('woocommerce_checkout_fields', array($this, 'wc_remove_checkout_fields'));
-
-    // pre-populate woo checkout field.
-    add_filter('woocommerce_checkout_get_value', array($this, 'woocommerce_checkout_get_value'), 10, 2);
-    
-    // if cart is empty, redirect to shop page
-    add_action('template_redirect', array($this, 'cart_empty_redirect_to_shop'));
     
     // ajax for removal
     add_action('wp_ajax_product_remove', array($this, 'custom_ajax_product_remove'));
@@ -83,45 +67,18 @@ class DirectToCheckout extends Timber {
     
     // replace checkout js. see wc_checkout_coupons->remove_coupon->line:669
     add_action('wp_head', array($this, 'direct_to_checkout_js'));
-  }
+    
+    // remove checkout fields
+    add_filter('woocommerce_checkout_fields', array($this, 'wc_remove_checkout_fields'));
 
-  public function add_to_twig($twig) { 
-    $twig->addExtension(new \Twig_Extension_StringLoader());
-    return $twig;
-  }
-
-  public function add_to_context($context) {
-    $context['plugin_url'] = DIRECT_TO_CHECKOUT_URL;
-    return $context;    
+    // pre-populate woo checkout field.
+    add_filter('woocommerce_checkout_get_value', array($this, 'woocommerce_checkout_get_value'), 10, 2);
+    
+    // if cart is empty, redirect to shop page
+    add_action('template_redirect', array($this, 'cart_empty_redirect_to_shop'));
   }
   
-  public function wc_get_template( $located, $template_name, $args, $template_path, $default_path ) {
-
-    if ( ! $default_path ) {
-      global $woocommerce;
-  		$default_path = $woocommerce->plugin_path() . '/templates/';
-  	}
-    
-    $plugin_path = DIRECT_TO_CHECKOUT_PATH.'woocommerce/' . $template_name;
-    
-    $woo_path = $default_path . $template_name;
-    
-    $theme_path = get_stylesheet_directory() . '/woocommerce/' . $template_name;
-    
-    if(@file_exists($theme_path) && @file_exists($plugin_path)){
-      $located = $plugin_path;
-    } elseif(@file_exists($theme_path) && !@file_exists($plugin_path)) {
-      $located = $theme_path;
-    } elseif(@file_exists($plugin_path) && !@file_exists($theme_path)) {
-      $located = $plugin_path;
-    } else {
-      $located = $woo_path;
-    }
-    
-    return $located;
-    
-  }
-  
+  // see on_plugins_loaded
   public function checkout_redirect_login() {
   
     global $woocommerce;
@@ -133,7 +90,6 @@ class DirectToCheckout extends Timber {
     }
   
   }
-
   public function redirect_to_checkout() {
   
   	global $woocommerce;
@@ -144,15 +100,6 @@ class DirectToCheckout extends Timber {
   	return $woocommerce->cart->get_checkout_url();
   
   }
-  
-  public function cart_empty_redirect_to_shop() {
-  	if (!is_checkout()) return;
-  	if (0 == WC()->cart->get_cart_contents_count()) {
-  		wp_safe_redirect( get_permalink(woocommerce_get_page_id('shop')) );
-  		exit;
-  	}
-  }
-  
   public function woo_checkout_remove_item($product_name, $cart_item, $cart_item_key) {
   	if ( is_checkout() ) {
   		$_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);      
@@ -169,7 +116,6 @@ class DirectToCheckout extends Timber {
   	}
   	return $product_name;
   }
-  
   public function custom_ajax_product_remove() {
     // Get order review fragment
     ob_start();
@@ -187,12 +133,10 @@ class DirectToCheckout extends Timber {
     woocommerce_order_review();
     $woocommerce_order_review = ob_get_clean();
   }
-
   public function direct_to_checkout_js() {
     global $wp_scripts;
     $wp_scripts->registered[ 'wc-checkout' ]->src = DIRECT_TO_CHECKOUT_URL . 'public/js/checkout.js';
   }
-  
   public function wc_remove_checkout_fields($fields) {
     
     // Billing fields
@@ -224,7 +168,6 @@ class DirectToCheckout extends Timber {
     // return thr remaining
     return $fields;
   }
-  
   public function woocommerce_checkout_get_value($input, $key) {
     global $current_user;
   
@@ -249,5 +192,50 @@ class DirectToCheckout extends Timber {
   
   	endswitch;
   }
+  public function cart_empty_redirect_to_shop() {
+  	if (!is_checkout()) return;
+  	if (0 == WC()->cart->get_cart_contents_count()) {
+  		wp_safe_redirect( get_permalink(woocommerce_get_page_id('shop')) );
+  		exit;
+  	}
+  }
+  
+  // override the template used for the checkout page. would be page.php from the theme but for this
+  public function direct_to_checkout_template($page_template) {
+    // if is the checkout page, use the new template
+    if (is_checkout()) {
+      $page_template = DIRECT_TO_CHECKOUT_PATH . 'templates/direct-to-checkout-template.php';
+    }
+    return $page_template;
+  }
+  
+  // that way they can be removed as in remove_custom_store_notice_from_plugin_on_checkout
+  public function remove_custom_store_notice_from_plugin_on_checkout() {
+    if(is_checkout()){
+      remove_action('rmcc_before_header', 'add_custom_demo_store_notice', 10);
+    }
+  }
+  
+  // allow plugin's woo templates to override others
+  public function wc_get_template($located, $template_name, $args, $template_path, $default_path) {
+    
+    // if defult path deosnt exists, set it
+    if (!$default_path) {
+      global $woocommerce;
+  		$default_path = $woocommerce->plugin_path() . '/templates/';
+  	}
 
+    $plugin_path = DIRECT_TO_CHECKOUT_PATH.'woocommerce/'.$template_name;
+    
+    // if the file exists in current plugin, set located to that
+    if(@file_exists($plugin_path)) {
+      $located = $plugin_path;
+    } elseif(@file_exists($located) && !@file_exists($plugin_path)) {
+      $located = $located;
+    } elseif(!@file_exists($located) && !@file_exists($plugin_path) && @file_exists($default_path)) {
+      $located = $default_path;
+    }
+
+    return $located;
+  }
 }
