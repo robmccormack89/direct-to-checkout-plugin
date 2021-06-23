09668c2ec19d15d0c4a1875c3afff29483c7e891
robmccormack89 <robertmccormack147@gmail.com>
2021-06-24 00:43:14 +0100

    Twig_Extension_StringLoader fix

--- inc/DirectToCheckout.php
+++ inc/DirectToCheckout.php
@@ -30,7 +30,9 @@ class DirectToCheckout extends Timber {
   
   // timber stuff. the usual stuff
   public function add_to_twig($twig) { 
-    $twig->addExtension(new \Twig_Extension_StringLoader());
+    if(!class_exists('Twig_Extension_StringLoader')){
+      $twig->addExtension(new Twig_Extension_StringLoader());
+    }
     return $twig;
   }
   public function add_to_context($context) {
