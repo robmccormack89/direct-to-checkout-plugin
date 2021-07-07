9d4c91c41d1d8cb7103be72d23274f24804b08d4
robmccormack89 <robertmccormack147@gmail.com>
2021-07-08 00:00:09 +0100

    format

--- inc/ProductRemove.php
+++ inc/ProductRemove.php
@@ -16,21 +16,19 @@ class ProductRemove {
   
   // allows removal of products in checkout
   public function woo_checkout_order_item_remove($product_name, $cart_item, $cart_item_key) {
-  	if (!is_checkout()) return;
+    if (!is_checkout()) return;
     
-		$_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);      
-		$product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
-		$remove_link = apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
+    $_product = apply_filters('woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key);      
+    $product_id = apply_filters('woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key);
+    $remove_link = apply_filters( 'woocommerce_cart_item_remove_link', sprintf(
       '<a href="%s" class="remove remove_from_cart_button_off" aria-label="%s" data-product_id="%s" data-cart_item_key="%s" data-product_sku="%s"><i class="fas fa-times"></i></a>',
       esc_url( wc_get_cart_remove_url($cart_item_key)),
       esc_attr_x('Remove this item', 'Checkout', 'direct-to-checkout'),
-      esc_attr($product_id),
-      esc_attr($cart_item_key),
       esc_attr($_product->get_sku())
     ), $cart_item_key );
-		$product_name = '<span>' . $remove_link . '</span><span class="uk-margin-small-left item-name">' . $_product->get_name() .'</span>';
-
-  	return $product_name;
+    $product_name = '<span>' . $remove_link . '</span><span class="uk-margin-small-left item-name">' . $_product->get_name() .'</span>';
+    
+    return $product_name;
   }
   
   // ajax for removal
